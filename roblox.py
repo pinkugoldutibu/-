7c427ea31e36aaa715e23782f69d2a764d3909e1
pinkugoldutibu <sharolarkansas@gmail.com>
2025-12-23 12:13:46 -0600

    Add files via upload

---
+++ roblox.py
@@ -0,0 +1,6 @@
+roblox = input("Do you play roblox?")
+if roblox =='yes':
+  nice = input("What is yout fav game?")
+  print("Nice choice it is very fun")
+else:
+  print("You shold give it a shot!")
